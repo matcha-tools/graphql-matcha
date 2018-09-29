0ab5764feb795e0fc9c5c1b12487560e162eb72d
Eddie <email@example.com>
2018-09-29 12:15:57 -0700

    edited unit test

--- cypress/integration/unit/unit.js
+++ cypress/integration/unit/unit.js
@@ -2,7 +2,7 @@ import { parseQueryArray } from '../../../src/helpers';
 
 describe("Unit tests", () => {
     it("should return query string from query array", () => {
-      let array = ["allFilms", "edges", "node", ["id", "filmID"]];
+      let array = ["allFilms", "edges", "node", ["id", "title"]];
       let expectedString = `{allFilms{edges{node{id title}}}}`;
       let result = parseQueryArray(array);
       expect(result).to.equal(expectedString);

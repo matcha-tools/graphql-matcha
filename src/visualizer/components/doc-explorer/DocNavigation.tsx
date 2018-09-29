7e7f658391d04406b5cacdab9f6c339c532c96da
JonathanChe <jonathan.che@outlook.com>
2018-09-29 16:14:44 -0700

    Update DocNavigation.tsx
    Removing console log

--- src/visualizer/components/doc-explorer/DocNavigation.tsx
+++ src/visualizer/components/doc-explorer/DocNavigation.tsx
@@ -25,7 +25,6 @@ class DocNavigation extends React.Component<DocNavigationProps> {
   render() {
     const { selectedType, previousType, dispatch, inQueryMode } = this.props;
 
-    console.log('checking to see what props are ', this.props)
     let clickHandler = () => {
       if (!previousType) return dispatch(clearSelection());
       // if in query mode, go back to original node and return edges as pending

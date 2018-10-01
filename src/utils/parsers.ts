eced4ec61994cda39401b592e15a2d2cdd092e75
seano <sth.xxx@gmail.com>
2018-10-01 12:25:02 -0700

    fixed the parser, updated the tests

--- src/utils/parsers.ts
+++ src/utils/parsers.ts
@@ -45,12 +45,11 @@ function braceRemainingElements(array:Array<any>, queryStr:string): string {
   for (let i = secondToLastIdx; i >=0; i--) {
     let element = array[i];
     let nextEle = array[i-1];
-    console.log('parsing ', element);
     queryStr = areFields(element)
     ? brace(element.join(" ") + queryStr)
     : !areFields(nextEle)
       ? brace(element + queryStr)
-      : element + queryStr;
+      : ' ' + element + queryStr;
   }
   return queryStr;
 }
